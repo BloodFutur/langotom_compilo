406d3e8dc193c10e3f98263e13ca86a97291af7c
Ronan <ronan.bonnet42@wanadoo.fr>
2024-04-10 22:49:50 +0200

    Clean code for ASM files.

--- symbol_table/asm.c
+++ symbol_table/asm.c
@@ -1,3 +1,14 @@
+/**
+ * @file asm.c
+ * @author Ronan Bonnet
+ * @author Anna Cazeneuve
+ * @brief  This file contains the implementation of the assembly code generation
+ * @version 0.1
+ * @date 2024-04-10
+ * @bug No known bugs
+ * @todo Add error handling
+ * @todo Add more functions such as if, while, etc. 
+ */
 #include "asm.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +16,25 @@
 #include "symbol_table.h"
 #include "instructions_table.h"
 
+/**
+ * @brief A macro to generate the assembly code for a binary operation
+ * 
+ * As the assembly code for binary operations is very similar, we can
+ * use a macro to generate the code for each operation. The macro takes
+ * two arguments: the name of the operation and the opcode of the operation.
+ * 
+ * @param instruction the name of the operation
+ * @param opcode the opcode of the operation
+ * 
+ * The macro generates a function with the name asm_instruction that takes
+ * four arguments: the line number in the code, the address of the first
+ * operand, the address of the second operand and the depth of the symbol table.
+ * Symbol table is used to store the result of the operation. If the operands
+ * are temporary variables, they are popped from the stack. The result is pushed
+ * on the stack. The instruction is inserted in the instruction table. The address
+ * of the result is returned.
+ * 
+ */
 #define ASSEMBLE_BINARY_OP(instruction, opcode) \
 int asm_##instruction(int line_number, int address1, int address2, int depth) {\
     printf("expression with t" #instruction " '%d' " #opcode " '%d'\n", address1, address2); \
@@ -15,59 +45,69 @@ int asm_##instruction(int line_number, int address1, int address2, int depth) {\
     return address; \
 }
 
+//
 // ARITHMETIC BINARY OPERATIONS
-ASSEMBLE_BINARY_OP(add, iADD)
-ASSEMBLE_BINARY_OP(sub, iSOU)
-ASSEMBLE_BINARY_OP(mul, iMUL)
-ASSEMBLE_BINARY_OP(div, iDIV)
+//
+ASSEMBLE_BINARY_OP(add, iADD) // Addition
+ASSEMBLE_BINARY_OP(sub, iSOU) // Subtraction
+ASSEMBLE_BINARY_OP(mul, iMUL) // Multiplication
+ASSEMBLE_BINARY_OP(div, iDIV) // Division
 
+//
 // LOGICAL BINARY OPERATIONS
-ASSEMBLE_BINARY_OP(eq, iEQ)
-ASSEMBLE_BINARY_OP(ne, iNEQ)
-ASSEMBLE_BINARY_OP(lt, iLT)
-ASSEMBLE_BINARY_OP(gt, iGT)
-ASSEMBLE_BINARY_OP(le, iLE)
-ASSEMBLE_BINARY_OP(ge, iGE)
-ASSEMBLE_BINARY_OP(and, iAND)
-ASSEMBLE_BINARY_OP(or, iOR)
+//
+ASSEMBLE_BINARY_OP(eq, iEQ)   // Equal
+ASSEMBLE_BINARY_OP(ne, iNEQ)  // Not equal
+ASSEMBLE_BINARY_OP(lt, iLT)   // Less than 
+ASSEMBLE_BINARY_OP(gt, iGT)   // Greater than
+ASSEMBLE_BINARY_OP(le, iLE)   // Less than or equal
+ASSEMBLE_BINARY_OP(ge, iGE)   // Greater than or equal
+ASSEMBLE_BINARY_OP(and, iAND) // Logical AND
+ASSEMBLE_BINARY_OP(or, iOR)   // Logical OR
 
-
+/* Number entry */
 int asm_nb(int line_number, int address1, int depth){
     printf("expression with tNB '%d'\n", address1);
+    
+    // Create a temporary symbol for the result
     int a = st_insert_tmp(address1, line_number, depth);
     st_print();
     it_insert(iAFC, a, address1, 0);
-    // fprintf(file,"AFC %d %d\n", a, address1);
     return a;
 }
 
+/* Variable assignment */
 void asm_assign(char* address1, int address2){
     printf("instruction with tID %s and expression %d\n", address1, address2);
+    
+    // Get the address of the variable to assign to
     int a=st_search(address1);
     it_insert(iCOP, a, address2, 0);
-    //fprintf(file,"COP %d %d\n", a, address2);
-    st_pop_tmp();
+    st_pop_tmp(); // Pop the result of the expression
     st_print();
 }
 
-
+/* Number negation */
 int asm_neg_nb(int line_number, int address1, int depth){
     printf("expression with tSUB\n");
+    // Create a temporary symbol for the result
     int address = st_insert_tmp(0, line_number, depth);
+    // Store 0 in the result
     it_insert(iAFC, address, 0 , 0);
+    // Subtract the number from 0(0 - number = -number)
     it_insert(iSOU, address, address, address1);
-    // fprintf(file,"SOU %d %d %d\n", address, address, address1);
     return address;
 }
 
+/* Number logical NOT */
 int asm_not(int line_number, int address1, int depth){
     printf("expression with tNOT\n");
     int address = st_insert_tmp(0, line_number, depth);
     it_insert(iNOT, address, 0, 0);
-    // fprintf(file,"NOT %d\n", address);
     return address;
 }
 
+/* Print instruction */
 void asm_print(int address1) {
     printf("instruction with tPRINT and expression\n");
     if(st_is_tmp(address1)) {st_pop_tmp();}
--- symbol_table/asm.h
+++ symbol_table/asm.h
@@ -1,37 +1,259 @@
+/**
+ * @file asm.h
+ * @author Ronan Bonnet
+ * @author Anna Cazeneuve
+ * @brief This file contains prototypes for the assembly code generation
+ * 
+ * This file contains the prototypes for the functions that generate the
+ * assembly code for the different operations of the language. It
+ * uses an instruction table to store the generated instructions. It also
+ * uses the symbol table to store the temporary variables and symbols such
+ * as variables, functions, etc.
+ *  
+ * @version 0.1
+ * @date 2024-04-10
+ * 
+ * @bug No known bugs
+ * @todo Add error handling
+ * 
+ */
 #ifndef ASM_H
 #define ASM_H
 
-
 #include <stdio.h>
 
-// @TODO : Check that a variable is declared before using it (search returns -1 if not found)
-// @TODO: LE and GE => create operator asm.
-// @TODO: Correct NEG number
 
-int asm_nb(int line_number, int address1, int depth);
+//
+// ARITHMETIC BINARY OPERATIONS
+//
 
-void asm_assign(char* address1, int address2);
 
+/**
+ * @brief Generate the assembly code for an addition
+ * 
+ * This function generates the assembly code for an addition
+ * operation. If the operands are temporary variables, they are
+ * popped from the stack. The result is pushed on the stack.
+ * The instruction is inserted in the instruction table. The 
+ * address of the result is returned.
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_add(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a subtraction
+ * 
+ * This function generates the assembly code for a subtraction.
+ * If the operands are temporary variables, they are popped from
+ * the stack. The result is pushed on the stack. The instruction
+ * is inserted in the instruction table. The address of the result
+ * is returned.
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_sub(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a multiplication
+ * 
+ * This function generates the assembly code for a multiplication.
+ * If the operands are temporary variables, they are popped from
+ * the stack. The result is pushed on the stack. The instruction
+ * is inserted in the instruction table. The address of the result
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_mul(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a division
+ * 
+ * This function generates the assembly code for a division.
+ * If the operands are temporary variables, they are popped from
+ * the stack. The result is pushed on the stack. The instruction
+ * is inserted in the instruction table. The address of the result
+ * 
+ * @note This function does not handle division by zero
+ * 
+ * @param line_number 
+ * @param address1 
+ * @param address2 
+ * @param depth 
+ * @return int 
+ */
 int asm_div(int line_number, int address1, int address2, int depth);
 
+
+//
+// LOGICAL BINARY OPERATIONS
+//
+
+
+/**
+ * @brief Generate the assembly code for an equal comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_eq(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a not equal comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_ne(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a less than comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_lt(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a greater than comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_gt(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a less than or equal comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_le(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a greater than or equal comparison
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_ge(int line_number, int address1, int address2, int depth);
 
+/**
+ * @brief Generate the assembly code for a logical AND operation
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
+int asm_and(int line_number, int address1, int address2, int depth);
+
+/**
+ * @brief Generate the assembly code for a logical OR operation
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the first operand
+ * @param address2      the address of the second operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
+int asm_or(int line_number, int address1, int address2, int depth);
+
+
+//
+// OTHER OPERATIONS
+// 
+
+
+/**
+ * @brief Generate the assembly code for a number entry
+ * 
+ * This function generates the assembly code for a number entry.
+ * Basically, it stores the number in a temporary variable in the
+ * symbols table and returns the address of the temporary variable.
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
+int asm_nb(int line_number, int address1, int depth);
+
+/**
+ * @brief Generate the assembly code for a variable assignment
+ * 
+ * This function generates the assembly code for a variable assignment.
+ * It assigns the value of the second operand to the first operand.
+ * 
+ * @param address1 the address of the first operand
+ * @param address2 the address of the second operand
+ */
+void asm_assign(char* address1, int address2);
+
+/**
+ * @brief Generate the assembly code for a negation
+ * 
+ * This function generates the assembly code for a negation.
+ * Basically, it creates a temporary variable in the symbol table
+ * with the value 0 and subtracts the operand from it.
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_neg_nb(int line_number, int address1, int depth);
+
+/**
+ * @brief Generate the assembly code for a logical NOT operation
+ * 
+ * @param line_number   the line number in the code
+ * @param address1      the address of the operand
+ * @param depth         the depth of the symbol table
+ * @return int the address of the result
+ */
 int asm_not(int line_number, int address1, int depth);
 
-int asm_and(int line_number, int address1, int address2, int depth);
-int asm_or( int line_number, int address1, int address2, int depth);
 
+/**
+ * @brief Generate the assembly code for a print operation
+ * 
+ * This function generates the assembly code for a print operation.
+ * It prints the value of the operand.
+ * 
+ * @param address1 the address of the value to print
+ */
 void asm_print(int address1);
-
-//void asm_if()
 
 #endif // ASM_H 
